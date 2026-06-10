8fa39da75dc448f55f5a3585ba60e626f2c8662e
SPICa27 <1286939372@qq.com>
2026-06-10 22:56:39 +0800

    chore: update versionName to 1.0_preview5 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 2
-        versionName = "1.0_preview4"
+        versionName = "1.0_preview5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
